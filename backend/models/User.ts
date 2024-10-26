0471136eee8cd533155450988c292bcc602e3473
Ultra-Kipen <hoozsay@gmail.com>
2024-10-26 11:51:41 +0900

    Update User.ts

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -14,71 +14,67 @@ class User extends Model {
   public readonly created_at!: Date;
   public readonly updated_at!: Date;
 
-  static getAttributes() {
-    return {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-      },
-      username: {
-        type: DataTypes.STRING(50),
-        allowNull: false,
-        unique: true,
-        validate: {
-          len: [3, 50]
+  static init(sequelize: Sequelize): void {
+    super.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        username: {
+          type: DataTypes.STRING(50),
+          allowNull: false,
+          unique: true,
+          validate: {
+            len: [3, 50]
+          }
+        },
+        email: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+          unique: true,
+          validate: {
+            isEmail: true
+          }
+        },
+        password: {
+          type: DataTypes.STRING(255),
+          allowNull: false
+        },
+        nickname: {
+          type: DataTypes.STRING(50),
+          allowNull: true
+        },
+        profile_image_url: {
+          type: DataTypes.STRING(255),
+          allowNull: true
+        },
+        theme_preference: {
+          type: DataTypes.ENUM('light', 'dark', 'system'),
+          allowNull: false,
+          defaultValue: 'system'
         }
       },
-      email: {
-        type: DataTypes.STRING(100),
-        allowNull: false,
-        unique: true,
-        validate: {
-          isEmail: true
-        }
-      },
-      password: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-      },
-      nickname: {
-        type: DataTypes.STRING(50),
-        allowNull: true
-      },
-      profile_image_url: {
-        type: DataTypes.STRING(255),
-        allowNull: true
-      },
-      theme_preference: {
-        type: DataTypes.ENUM('light', 'dark', 'system'),
-        allowNull: false,
-        defaultValue: 'system'
-      }
-    };
-  }
-
-  static getTableName() {
-    return 'users';
-  }
-
-  static getOptions() {
-    return {
-      hooks: {
-        beforeSave: async (user: User) => {
-          if (user.password) {
-            const salt = await bcrypt.genSalt(10);
-            user.password = await bcrypt.hash(user.password, salt);
+      {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+        timestamps: true,
+        underscored: true,
+        hooks: {
+          beforeSave: async (user: User) => {
+            if (user.changed('password')) {
+              const salt = await bcrypt.genSalt(10);
+              user.password = await bcrypt.hash(user.password, salt);
+            }
           }
         }
       }
-    };
+    );
   }
 
-  async validatePassword(password: string): Promise<boolean> {
-    return bcrypt.compare(password, this.password);
-  }
-
-  static associate(models: any) {
+  static associate(models: any): void {
     this.hasMany(models.MyDayPost, {
       foreignKey: 'user_id',
       as: 'myDayPosts'
@@ -88,21 +84,10 @@ class User extends Model {
       foreignKey: 'user_id',
       as: 'someoneDayPosts'
     });
+  }
 
-    this.hasMany(models.Challenge, {
-      foreignKey: 'creator_id',
-      as: 'createdChallenges'
-    });
-
-    this.hasOne(models.UserStats, {
-      foreignKey: 'user_id',
-      as: 'stats'
-    });
-
-    this.hasMany(models.Notification, {
-      foreignKey: 'user_id',
-      as: 'notifications'
-    });
+  async validatePassword(password: string): Promise<boolean> {
+    return bcrypt.compare(password, this.password);
   }
 }
 
