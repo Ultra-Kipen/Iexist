986f43ffc547c1f520fe20707445008ea21dabef
Ultra-Kipen <hoozsay@gmail.com>
2024-10-26 12:09:09 +0900

    Update Challenge.ts

--- backend/models/Challenge.ts
+++ backend/models/Challenge.ts
@@ -1,99 +1,130 @@
+// backend/models/Challenge.ts
+
 import { Model, DataTypes, Sequelize } from 'sequelize';
 
-interface ChallengeAttributes {
-  challenge_id: number;
-  creator_id: number;
-  title: string;
-  description?: string;
-  start_date: Date;
-  end_date: Date;
-  is_public: boolean;
-  max_participants?: number;
-  participant_count: number;
-  created_at: Date;
-}
-
-class Challenge extends Model<ChallengeAttributes> {
-  public challenge_id!: number;
+class Challenge extends Model {
+  public id!: number;
   public creator_id!: number;
   public title!: string;
-  public description!: string | undefined;
+  public description!: string;
   public start_date!: Date;
   public end_date!: Date;
   public is_public!: boolean;
-  public max_participants!: number | undefined;
+  public max_participants?: number;
   public participant_count!: number;
-  public created_at!: Date;
+  public readonly created_at!: Date;
+  public readonly updated_at!: Date;
 
-  static initModel(sequelize: Sequelize): typeof Challenge {
-    Challenge.init({
-      challenge_id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+  static init(sequelize: Sequelize): void {
+    super.init(
+      {
+        id: {
+          type: DataTypes.INTEGER,
+          autoIncrement: true,
+          primaryKey: true,
+        },
+        creator_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          references: {
+            model: 'users',
+            key: 'id'
+          }
+        },
+        title: {
+          type: DataTypes.STRING(100),
+          allowNull: false,
+          validate: {
+            len: [5, 100]
+          }
+        },
+        description: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+          validate: {
+            len: [20, 500]
+          }
+        },
+        start_date: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          validate: {
+            isDate: true,
+            isAfter: new Date().toString() // 시작일은 현재 날짜 이후여야 함
+          }
+        },
+        end_date: {
+          type: DataTypes.DATEONLY,
+          allowNull: false,
+          validate: {
+            isDate: true,
+            isAfterStart(value: Date) {
+              if (value <= this.start_date) {
+                throw new Error('종료일은 시작일 이후여야 합니다.');
+              }
+            }
+          }
+        },
+        is_public: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+          defaultValue: true
+        },
+        max_participants: {
+          type: DataTypes.INTEGER,
+          allowNull: true,
+          validate: {
+            min: 1
+          }
+        },
+        participant_count: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          defaultValue: 0
+        }
       },
-      creator_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
-      title: {
-        type: DataTypes.STRING(100),
-        allowNull: false
-      },
-      description: DataTypes.TEXT,
-      start_date: {
-        type: DataTypes.DATE,
-        allowNull: false
-      },
-      end_date: {
-        type: DataTypes.DATE,
-        allowNull: false
-      },
-      is_public: {
-        type: DataTypes.BOOLEAN,
-        defaultValue: true
-      },
-      max_participants: DataTypes.SMALLINT.UNSIGNED,
-      participant_count: {
-        type: DataTypes.SMALLINT.UNSIGNED,
-        defaultValue: 0
-      },
-      created_at: {
-        type: DataTypes.DATE,
-        defaultValue: DataTypes.NOW
+      {
+        sequelize,
+        modelName: 'Challenge',
+        tableName: 'challenges',
+        timestamps: true,
+        underscored: true,
+        indexes: [
+          {
+            fields: ['creator_id']
+          },
+          {
+            fields: ['start_date', 'end_date']
+          },
+          {
+            fields: ['is_public']
+          }
+        ]
       }
-    }, {
-      sequelize,
-      modelName: 'Challenge',
-      tableName: 'challenges',
-      timestamps: true,
-      underscored: true
-    });
-
-    return Challenge;
+    );
   }
 
   static associate(models: any): void {
-    Challenge.belongsTo(models.User, {
-      as: 'Creator',
-      foreignKey: 'creator_id'
+    // Creator와의 관계
+    this.belongsTo(models.User, {
+      foreignKey: 'creator_id',
+      as: 'creator'
     });
-    // ChallengeParticipant와 ChallengeEmotion 모델이 정의된 후에만 관계 설정
-    if (models.ChallengeParticipant) {
-      Challenge.hasMany(models.ChallengeParticipant, {
-        foreignKey: 'challenge_id',
-        as: 'Participants'
-      });
-    }
-    if (models.ChallengeEmotion) {
-      Challenge.hasMany(models.ChallengeEmotion, {
-        foreignKey: 'challenge_id',
-        as: 'Emotions'
-      });
-    }
+
+    // Participant와의 다대다 관계
+    this.belongsToMany(models.User, {
+      through: models.ChallengeParticipant,
+      foreignKey: 'challenge_id',
+      otherKey: 'user_id',
+      as: 'participants'
+    });
+
+    // ChallengeEmotion과의 관계
+    this.hasMany(models.ChallengeEmotion, {
+      foreignKey: 'challenge_id',
+      as: 'emotions'
+    });
   }
 }
 
-export default (sequelize: Sequelize): typeof Challenge => {
-  return Challenge.initModel(sequelize);
-};
+export default Challenge;
