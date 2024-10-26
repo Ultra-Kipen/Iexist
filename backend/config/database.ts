52ccfe67458752b43a53a9cefa8b546a2e6a2bad
Ultra-Kipen <hoozsay@gmail.com>
2024-10-26 12:50:55 +0900

    Update database.ts

--- backend/config/database.ts
+++ backend/config/database.ts
@@ -5,29 +5,31 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const sequelize = new Sequelize({
-  database: process.env.DB_NAME || 'iexist',
-  username: process.env.DB_USER || 'Iexist',
-  password: process.env.DB_PASSWORD || 'sw309824!@',
-  host: process.env.DB_HOST || 'localhost',
-  port: Number(process.env.DB_PORT) || 3306,
-  dialect: 'mysql',
-  timezone: '+09:00',
-  define: {
-    charset: 'utf8mb4',
-    collate: 'utf8mb4_general_ci',
-    underscored: true,
-    freezeTableName: false,
-    timestamps: true,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  logQueryParameters: process.env.NODE_ENV === 'development',
-  logging: process.env.NODE_ENV === 'development' ? console.log : false,
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME || 'iexist',
+  process.env.DB_USER || 'Iexist',
+  process.env.DB_PASSWORD || 'sw309824!@',
+  {
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+    timezone: '+09:00',
+    define: {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci',
+      underscored: true,
+      freezeTableName: false,
+      timestamps: true,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    logQueryParameters: process.env.NODE_ENV === 'development',
+    logging: process.env.NODE_ENV === 'development' ? console.log : false,
+  }
+);
 
-export { sequelize, Sequelize };
+export default sequelize;
