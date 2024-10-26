c5873783d94621e543197182d9e7ad44c4f63478
Ultra-Kipen <hoozsay@gmail.com>
2024-10-26 12:27:15 +0900

    Update User.ts

--- backend/models/User.ts
+++ backend/models/User.ts
@@ -1,94 +1,64 @@
-// backend/models/User.ts
+// backend/models/index.ts
 
-import { Model, DataTypes, Sequelize } from 'sequelize';
-import bcrypt from 'bcrypt';
+import { Sequelize } from 'sequelize';
+import sequelize from '../config/database';
 
-class User extends Model {
-  public id!: number;
-  public username!: string;
-  public email!: string;
-  public password!: string;
-  public nickname?: string;
-  public profile_image_url?: string;
-  public theme_preference!: 'light' | 'dark' | 'system';
-  public readonly created_at!: Date;
-  public readonly updated_at!: Date;
+import User from './User';
+import Challenge from './Challenge';
+import Emotion from './Emotion';
+import EmotionLog from './EmotionLog';
+import MyDayComment from './MyDayComment';
+import MyDayPost from './MyDayPost';
+import SomeoneDayPost from './SomeoneDayPost';
+import Tag from './Tag';
+import MyDayLike from './MyDayLike';
+import SomeoneDayComment from './SomeoneDayComment';
+import SomeoneDayLike from './SomeoneDayLike';
+import ChallengeParticipant from './ChallengeParticipant';
+import ChallengeEmotion from './ChallengeEmotion';
+import UserStats from './UserStats';
+import Notification from './Notification';
+import PostReport from './PostReport';
+import EncouragementMessage from './EncouragementMessage';
 
-  static init(sequelize: Sequelize): void {
-    super.init(
-      {
-        id: {
-          type: DataTypes.INTEGER,
-          autoIncrement: true,
-          primaryKey: true,
-        },
-        username: {
-          type: DataTypes.STRING(50),
-          allowNull: false,
-          unique: true,
-          validate: {
-            len: [3, 50]
-          }
-        },
-        email: {
-          type: DataTypes.STRING(100),
-          allowNull: false,
-          unique: true,
-          validate: {
-            isEmail: true
-          }
-        },
-        password: {
-          type: DataTypes.STRING(255),
-          allowNull: false
-        },
-        nickname: {
-          type: DataTypes.STRING(50),
-          allowNull: true
-        },
-        profile_image_url: {
-          type: DataTypes.STRING(255),
-          allowNull: true
-        },
-        theme_preference: {
-          type: DataTypes.ENUM('light', 'dark', 'system'),
-          allowNull: false,
-          defaultValue: 'system'
-        }
-      },
-      {
-        sequelize,
-        modelName: 'User',
-        tableName: 'users',
-        timestamps: true,
-        underscored: true,
-        hooks: {
-          beforeSave: async (user: User) => {
-            if (user.changed('password')) {
-              const salt = await bcrypt.genSalt(10);
-              user.password = await bcrypt.hash(user.password, salt);
-            }
-          }
-        }
-      }
-    );
+const db = {
+  sequelize,
+  Sequelize,
+  User: User.init({
+    // User model attributes here
+  }, { sequelize }),
+  Challenge: Challenge.init({
+    // Challenge model attributes here
+  }, { sequelize }),
+  // ... other models initialization
+};
+
+Object.values(db).forEach((model: any) => {
+  if (model.associate) {
+    model.associate(db);
   }
+});
 
-  static associate(models: any): void {
-    this.hasMany(models.MyDayPost, {
-      foreignKey: 'user_id',
-      as: 'myDayPosts'
-    });
+export type DbInterface = {
+  sequelize: Sequelize;
+  Sequelize: typeof Sequelize;
+  User: typeof User;
+  Challenge: typeof Challenge;
+  Emotion: typeof Emotion;
+  EmotionLog: typeof EmotionLog;
+  MyDayComment: typeof MyDayComment;
+  MyDayPost: typeof MyDayPost;
+  SomeoneDayPost: typeof SomeoneDayPost;
+  Tag: typeof Tag;
+  MyDayLike: typeof MyDayLike;
+  SomeoneDayComment: typeof SomeoneDayComment;
+  SomeoneDayLike: typeof SomeoneDayLike;
+  ChallengeParticipant: typeof ChallengeParticipant;
+  ChallengeEmotion: typeof ChallengeEmotion;
+  UserStats: typeof UserStats;
+  Notification: typeof Notification;
+  PostReport: typeof PostReport;
+  EncouragementMessage: typeof EncouragementMessage;
+};
 
-    this.hasMany(models.SomeoneDayPost, {
-      foreignKey: 'user_id',
-      as: 'someoneDayPosts'
-    });
-  }
-
-  async validatePassword(password: string): Promise<boolean> {
-    return bcrypt.compare(password, this.password);
-  }
-}
-
-export default User;
+export default db as DbInterface;
